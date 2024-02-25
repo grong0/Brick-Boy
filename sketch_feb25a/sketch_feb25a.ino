d66ee1c2c31e9c5975eaa9f0edfaa99898f7c121
Garrett59100 <garrett59100@gmail.com>
2024-02-25 04:50:11 -0500

    Some changes of all time

--- sketch_feb25a/sketch_feb25a.ino
+++ sketch_feb25a/sketch_feb25a.ino
@@ -1,11 +1,11 @@
 #include <SPI.h>
-#include <TFT_eSPI.h> // Hardware-specific library
+#include <TFT_eSPI.h>  // Hardware-specific library
 
-TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+TFT_eSPI tft = TFT_eSPI();	// Invoke custom library
 
-//#define GRIDX 80
-//#define GRIDY 60
-//#define CELLXY 4
+// #define GRIDX 80
+// #define GRIDY 60
+// #define CELLXY 4
 
 #define GRIDX 160
 #define GRIDY 106
@@ -13,74 +13,67 @@ TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
 
 #define GEN_DELAY 0
 
-//Current grid
+// Current grid
 uint8_t grid[GRIDX][GRIDY];
 
-//The new grid for the next generation
+// The new grid for the next generation
 uint8_t newgrid[GRIDX][GRIDY];
 
-//Number of generations
+// Number of generations
 #define NUMGEN 600
 
 uint16_t genCount = 0;
 enum State {
-  MainMenu,
-  TetMenu,
-  TetGame,
-  TetGameOver
+	MainMenu,
+	TetMenu,
+	TetGame,
+	TetGameOver
 };
 
 int level = 0;
 int score = 0;
 int blocks[20][10];
-//0, 1 = I, 2 = L, 3 = J, 4 = Z, 5 = S, 6 = o, 7 = T
+// 0, 1 = I, 2 = L, 3 = J, 4 = Z, 5 = S, 6 = o, 7 = T
 
 int curBlock[0][0];
-
-
 
 State state;
 
 bool buttons[6];
 
-void setup(){
-  tft.init();
-  tft.setRotation(0);
-  tft.fillScreen(TFT_BLACK);
-  state = Menu;
+void setup() {
+	tft.init();
+	tft.setRotation(0);
+	tft.fillScreen(TFT_BLACK);
+	state = MainMenu;
 
-  for(int i = 2; i < 8; i++){
-    pinMode(i, INPUT_PULLDOWN);
-    buttons[i - 2] = false;
-  }
+	for (int i = 2; i < 8; i++) {
+		pinMode(i, INPUT_PULLDOWN);
+		buttons[i - 2] = false;
+	}
 }
 
 void loop() {
-  for(int i = 2; i < 8; i++){
-    digitalRead(i)
-  }
+	for (int i = 2; i < 8; i++) {
+		digitalRead(i);
+	}
 
+	switch (state) {
+		case MainMenu:
+			break;
 
+		case TetMenu:
+			break;
 
+		case TetGame:
+			break;
 
-  switch(state){
-    case State.MainMenu:
-
-      break;
-    case State.TetMenu:
-
-      break;
-    case State.TetGame:
-
-      break;
-    case State.TetGameOver:
-      break;
-  }
+		case TetGameOver:
+			break;
+	}
 }
 
-
 void startTetris() {
-  level = 0;
-  score = 0;
-
+	level = 0;
+	score = 0;
 }
